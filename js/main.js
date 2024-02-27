9e6727d834ec72a98db27175644884ec75c9b37d
Богдан Троценко <trotsbv@gmail.com>
2024-02-27 22:38:12 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,37 +1,62 @@
-const PHOTO_DESCRIPTIONS =
-[
-  'поле', 'табличка', 'озеро', 'девушка', 'шоколад',
-  'черная машина', 'клубника', 'стакан', 'самолёт', 'ботинки',
-  'песок', 'белая машина', 'мясо', 'кот','валенки',
-  'небо', 'хор', 'кирпич', 'фонарик', 'пальма',
-  'вилка', 'море', 'краб', 'концерт', 'бегемот'
+const PHOTO_DESCRIPTIONS = [
+  'поле',
+  'табличка',
+  'озеро',
+  'девушка',
+  'шоколад',
+  'черная машина',
+  'клубника',
+  'стакан',
+  'самолёт',
+  'ботинки',
+  'песок',
+  'белая машина',
+  'мясо',
+  'кот',
+  'валенки',
+  'небо',
+  'хор',
+  'кирпич',
+  'фонарик',
+  'пальма',
+  'вилка',
+  'море',
+  'краб',
+  'концерт',
+  'бегемот',
 ];
 
-const COMMENT_MESSAGES =
-[
+const COMMENT_MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-const USER_NAMES =
-[
-  'Иван','Андрей','Дима', 'Анна', 'Ольга',
-  'Мария', 'Анастасия', 'Евгений', 'Геннадий', 'Леопольд'
+const USER_NAMES = [
+  'Иван',
+  'Андрей',
+  'Дима',
+  'Анна',
+  'Ольга',
+  'Мария',
+  'Анастасия',
+  'Евгений',
+  'Геннадий',
+  'Леопольд',
 ];
 
-
-const getRandomInteger = (min,max) => {
-  const lower = Math.ceil(Math.min(min,max));
-  const upper = Math.floor(Math.max(min,max));
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
 
-const getRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+const getRandomElement = (elements) =>
+  elements[getRandomInteger(0, elements.length - 1)];
 
 const createIdGenerator = (min, max, uniqueness = false) => {
   const storage = [];
@@ -39,7 +64,7 @@ const createIdGenerator = (min, max, uniqueness = false) => {
   return () => {
     let randomValue = getRandomInteger(min, max);
 
-    if (uniqueness){
+    if (uniqueness) {
       while (storage.includes(randomValue)) {
         randomValue = getRandomInteger(min, max);
       }
@@ -49,45 +74,33 @@ const createIdGenerator = (min, max, uniqueness = false) => {
   };
 };
 
-const generateMessage = (value, storageMessage) => {
-  const message = [];
-  for(let i = 0; i < value; i++){
-    let sentence = getRandomElement(storageMessage);
-    while(message.includes(sentence)) {
-      sentence = getRandomElement(storageMessage);
-    }
-    message.push(sentence);
-  }
-  return message.join(' ');
-};
+const getPhotoId = createIdGenerator(1, 25, true);
+const getCommentId = createIdGenerator(1, 1_000, true);
 
-const GENERATION_PARAMETERS = {
-  getId : createIdGenerator(1,25, true),
-  getUrl : createIdGenerator(1,25, true),
-  getDescription : createIdGenerator(1,25),
-  getCommentId : createIdGenerator(1,1000, true)
-};
+const generateDescription = () => getRandomElement(PHOTO_DESCRIPTIONS);
 
-const generateComments = (value) => {
-  const comments = [];
-  for (let i = 0; i < value; i ++) {
-    comments.push({
-      id: GENERATION_PARAMETERS.getCommentId(),
-      avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
-      message: generateMessage (Math.ceil(Math.random() * 2), COMMENT_MESSAGES),
-      name: getRandomElement(USER_NAMES)
-    });
-  }
-  return comments;
-};
+const generateMessage = () =>
+  Array.from({ length: getRandomInteger(1, 2) }, () =>
+    getRandomElement(COMMENT_MESSAGES)
+  ).join(' ');
 
-const createDescriptionImage = () => ({
-  id: GENERATION_PARAMETERS.getId(),
-  url: `photos/${GENERATION_PARAMETERS.getUrl()}.jpg`,
-  description: PHOTO_DESCRIPTIONS[GENERATION_PARAMETERS.getDescription()],
-  likes: getRandomInteger(15,200),
-  comments: generateComments(getRandomInteger(0,30))
+const generateComments = () => ({
+  id: getCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: generateMessage(),
+  name: getRandomElement(USER_NAMES),
 });
 
+const createDescriptionImage = () => {
+  const id = getPhotoId();
 
-createDescriptionImage();
+  return {
+    id,
+    url: `photos/${id}.jpg`,
+    description: generateDescription(),
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({ length: getRandomInteger(0, 30) }, generateComments),
+  };
+};
+
+export { createDescriptionImage };
