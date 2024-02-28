ea5c0eb73595a29fa81bdf6d04fa3df1e45d4fc9
Богдан Троценко <trotsbv@gmail.com>
2024-02-29 00:06:28 +0300

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,45 @@
-//1
-const checkLength = (string, length) => string.length <= length;
+// //1
+// const checkLength = (string, length) => string.length <= length;
 
-//2
-const checkPalindrome = (string) => {
-  const formattedString = string.replaceAll(' ', '').toLowerCase();
-  let reverseString = '';
-  for (let i = formattedString.length - 1; i >= 0; i--) {
-    reverseString += formattedString.at(i);
-  }
-  return formattedString === reverseString;
+// //2
+// const checkPalindrome = (string) => {
+//   const formattedString = string.replaceAll(' ', '').toLowerCase();
+//   let reverseString = '';
+//   for (let i = formattedString.length - 1; i >= 0; i--) {
+//     reverseString += formattedString.at(i);
+//   }
+//   return formattedString === reverseString;
+// };
+
+// //3
+// const convertToNumber = (string) => {
+//   string = string.toString();
+//   let finalNumber = '';
+
+//   for (let i = 0; i <= string.length; i++) {
+//     const char = string.at(i);
+
+//     if (parseInt(char, 10) || (char === '0' && finalNumber.length !== 0)) {
+//       finalNumber += string.at(i);
+//     }
+//   }
+
+//   return finalNumber.length > 0 ? finalNumber : NaN;
+// };
+
+const checkMeetingDuration = (workStart, workEnd, meetStart, duration) => {
+  duration = `0:${duration}`;
+
+  const times = [workStart, workEnd, meetStart, duration];
+
+  const timeInMinutes = times.map((item) => {
+    const time = item.split(':');
+    return time[0] * 60 + +time[1];
+  });
+
+  const meetingTime = timeInMinutes[2] + timeInMinutes[3];
+
+  return timeInMinutes[0] <= timeInMinutes[2] && meetingTime <= timeInMinutes[1];
 };
 
-//3
-const convertToNumber = (string) => {
-  string = string.toString();
-  let finalNumber = '';
-
-  for (let i = 0; i <= string.length; i++) {
-    const char = string.at(i);
-
-    if (parseInt(char, 10) || (char === '0' && finalNumber.length !== 0)) {
-      finalNumber += string.at(i);
-    }
-  }
-
-  return finalNumber.length > 0 ? finalNumber : NaN;
-};
-
-checkLength('abc', 2);
-checkPalindrome('bob');
-convertToNumber('1b');
+export { checkMeetingDuration };
