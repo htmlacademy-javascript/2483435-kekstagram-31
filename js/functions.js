3fe3672bbda5789e3643ab287af8ef0328a76c6d
Богдан <159007823+b-trots@users.noreply.github.com>
2024-02-28 16:06:45 +0300

    Больше деталей (#4)
    ---------
    
    Co-authored-by: Богдан Троценко <trotsbv@gmail.com>

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,32 @@
-
 //1
 const checkLength = (string, length) => string.length <= length;
-
 
 //2
 const checkPalindrome = (string) => {
   const formattedString = string.replaceAll(' ', '').toLowerCase();
   let reverseString = '';
-  for (let i = formattedString.length - 1;i >= 0;i--) {
+  for (let i = formattedString.length - 1; i >= 0; i--) {
     reverseString += formattedString.at(i);
   }
   return formattedString === reverseString;
 };
 
-
 //3
 const convertToNumber = (string) => {
-
   string = string.toString();
   let finalNumber = '';
 
   for (let i = 0; i <= string.length; i++) {
     const char = string.at(i);
 
-    if ((parseInt(char, 10)) || char === '0' && finalNumber.length !== 0) {
+    if (parseInt(char, 10) || (char === '0' && finalNumber.length !== 0)) {
       finalNumber += string.at(i);
     }
   }
 
   return finalNumber.length > 0 ? finalNumber : NaN;
 };
+
+checkLength('abc', 2);
+checkPalindrome('bob');
+convertToNumber('1b');
