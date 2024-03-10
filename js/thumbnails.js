47230a5ff9dcdfd7da36c3260a690ceb209c77ab
Богдан Троценко <trotsbv@gmail.com>
2024-03-11 00:45:23 +0300

    Избавляюсь от повторного цикла для обработчика клика

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -3,27 +3,41 @@ import { openModal } from './showFullPhoto.js';
 
 const createThumbnails = createDescriptionImages();
 const container = document.querySelector('.pictures');
-const template = document.querySelector('#picture').content.querySelector('.picture');
+const template = document
+  .querySelector('#picture')
+  .content.querySelector('.picture');
 const newFragment = document.createDocumentFragment();
 
-createThumbnails.forEach(({ id, url, description, likes, comments }) => {
-  const thumbnail = template.cloneNode(true);
-  thumbnail.href = `/photos/${id}.jpg`;
-  thumbnail.querySelector('.picture__img').src = url;
-  thumbnail.querySelector('.picture__img').alt = description;
-  thumbnail.querySelector('.picture__likes').textContent = likes;
-  thumbnail.querySelector('.picture__comments').textContent = comments.length;
-  newFragment.append(thumbnail);
-});
+const onClickThumbnails = () => {
+  createThumbnails.forEach((item) => {
+    const thumbnail = template.cloneNode(true);
+    thumbnail.href = `/photos/${item.id}.jpg`;
+    thumbnail.querySelector('.picture__img').src = item.url;
+    thumbnail.querySelector('.picture__img').alt = item.description;
+    thumbnail.querySelector('.picture__likes').textContent = item.likes;
+    thumbnail.querySelector('.picture__comments').textContent =
+      item.comments.length;
+    newFragment.append(thumbnail);
+  });
 
-container.append(newFragment);
+  container.append(newFragment);
 
+  addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('picture__img')) {
+      evt.preventDefault();
+      console.log(createThumbnails.filter((item) => item.id === Number(evt.target.src.match(/\d+/g)[1])));
+      openModal(createThumbnails.filter((item) => item.id === Number(evt.target.src.match(/\d+/g)[1])));
+    }
+  });
+};
 
-const targetThumbnails = container.querySelectorAll('.picture__img');
+onClickThumbnails();
 
-for (let i = 0; i < targetThumbnails.length; i++){
+// const targetThumbnails = container.querySelectorAll('.picture__img');
+
+/* for (let i = 0; i < targetThumbnails.length; i++){
   targetThumbnails[i].addEventListener('click', (evt) => {
     evt.preventDefault();
     openModal(createThumbnails[i]);
   });
-}
+} */
