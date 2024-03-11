48201f861f89c6cc1eacfaabc1036bf7e3553452
Богдан Троценко <trotsbv@gmail.com>
2024-03-12 00:34:42 +0300

    Проблема с циклом вывода комментариев

--- js/showFullPhoto.js
+++ js/showFullPhoto.js
@@ -6,28 +6,67 @@ const commentsListItem = commentsList.querySelector('.social__comment');
 const commentsCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
 const closeModalButton = document.querySelector('.big-picture__cancel');
+const countShownComments = commentsCount.querySelector(
+  '.social__comment-shown-count'
+);
 
 const showBigPhoto = ({ url, likes, comments, description }) => {
   bigPhotoModal.querySelector('.big-picture__img img').src = url;
   bigPhotoModal.querySelector('.likes-count').textContent = likes;
-  bigPhotoModal.querySelector('.social__comment-shown-count').textContent = comments.length;
+  bigPhotoModal.querySelector('.social__comment-shown-count').textContent =
+    comments.length < 5 ? comments.length : 5;
   bigPhotoModal.querySelector('.social__comment-total-count').textContent =
     comments.length;
   bigPhotoModal.querySelector('.social__caption').textContent = description;
 };
 
+
 const showBigPhotoComments = (comments) => {
   commentsList.innerHTML = '';
-  comments.forEach(({ message, avatar, name }) => {
+  comments.forEach((item, index) => {
     const currentComment = commentsListItem.cloneNode(true);
     const userOfCurrentComment =
       currentComment.querySelector('.social__picture');
-    currentComment.querySelector('.social__text').textContent = message;
-    userOfCurrentComment.src = avatar;
-    userOfCurrentComment.alt = name;
+    currentComment.querySelector('.social__text').textContent = item.message;
+    userOfCurrentComment.src = item.avatar;
+    userOfCurrentComment.alt = item.name;
+    if (index > 4) {
+      currentComment.classList.add('hidden');
+    }
     commentsList.append(currentComment);
   });
+  toShowComments(countShownComments);
 };
+
+function toShowComments(countShownComments) {
+
+  if (commentsList.children.length <= 5) {
+    commentsLoader.classList.add('hidden');
+  }
+
+  const toRenderComments = () => {
+    if (+countShownComments.textContent > commentsList.children.length) {
+      countShownComments.textContent = commentsList.children.length;
+      commentsLoader.classList.add('hidden');
+    }
+    for (let i = 0; i < commentsList.children.length; i++) {
+      if (i < countShownComments.textContent) {
+        commentsList.children[i].classList.remove('hidden');
+      }
+    }
+
+    if (+countShownComments.textContent >= commentsList.children.length) {
+      countShownComments.textContent = commentsList.children.length;
+      commentsLoader.classList.add('hidden');
+    }
+  };
+
+  commentsLoader.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    countShownComments.textContent = +countShownComments.textContent + +5;
+    toRenderComments();
+  });
+}
 
 const renderBigPhoto = (targetThumbnail) => {
   bigPhotoModal.classList.remove('hidden');
@@ -48,6 +87,7 @@ const onDocumentKeyDown = (evt) => {
 
 function closeModal() {
   bigPhotoModal.classList.add('hidden');
+  commentsLoader.classList.remove('hidden');
   blockScrolling();
   document.removeEventListener('keydown', onDocumentKeyDown);
 }
