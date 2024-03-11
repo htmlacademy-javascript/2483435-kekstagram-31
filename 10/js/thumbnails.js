cc95a34c115f1d6ccb144c1567e624688b7b845e
Keks <keks@htmlacademy.ru>
2024-03-11 12:33:14 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/thumbnails.js
+++ 10/js/thumbnails.js
@@ -1,29 +1,26 @@
 import { createDescriptionImages } from './data.js';
 import { openModal } from './showFullPhoto.js';
 
-const createThumbnails = createDescriptionImages();
+const createdThumbnails = createDescriptionImages();
 const container = document.querySelector('.pictures');
-const template = document.querySelector('#picture').content.querySelector('.picture');
+const template = document
+  .querySelector('#picture')
+  .content.querySelector('.picture');
 const newFragment = document.createDocumentFragment();
 
-createThumbnails.forEach(({ id, url, description, likes, comments }) => {
+createdThumbnails.forEach((item) => {
   const thumbnail = template.cloneNode(true);
-  thumbnail.href = `/photos/${id}.jpg`;
-  thumbnail.querySelector('.picture__img').src = url;
-  thumbnail.querySelector('.picture__img').alt = description;
-  thumbnail.querySelector('.picture__likes').textContent = likes;
-  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.href = `/photos/${item.id}.jpg`;
+  thumbnail.querySelector('.picture__img').src = item.url;
+  thumbnail.querySelector('.picture__img').alt = item.description;
+  thumbnail.querySelector('.picture__likes').textContent = item.likes;
+  thumbnail.querySelector('.picture__comments').textContent =
+    item.comments.length;
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openModal(item);
+  });
   newFragment.append(thumbnail);
 });
 
 container.append(newFragment);
-
-
-const targetThumbnails = container.querySelectorAll('.picture__img');
-
-for (let i = 0; i < targetThumbnails.length; i++){
-  targetThumbnails[i].addEventListener('click', (evt) => {
-    evt.preventDefault();
-    openModal(createThumbnails[i]);
-  });
-}
