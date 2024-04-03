7edd1b497f3063eab8297eb2af7fa20d81049dab
Arthur Baileys Li <arthurlitovko@gmail.com>
2024-04-03 16:44:46 +0300

    Chore: delete unused vendor and setup build

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+/** @type {import('vite').UserConfig} */
+export default {
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 3000,
+  },
+};
